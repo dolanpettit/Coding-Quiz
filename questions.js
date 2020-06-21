d9a8698e72092da4493c19259ad284985e461845
Dolan Pettit <dolanpettit@gmail.com>
2020-06-20 21:24:21 -0400

    Updating questions.js with the array of question objects

---
+++ questions.js
@@ -0,0 +1,38 @@
+var questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+    answer: 2,
+  },
+  {
+    question:
+      "String values must be enclosed within _______ when being assigned to variables.",
+    choices: ["Quotes", "Curly Brackets", "Commas", "Parentheses"],
+    answer: 0,
+  },
+
+  {
+    question:
+      "The condition in an if / else statement is enclosed within _______.",
+    choices: ["Quotes", "Parentheses", "Square Brackets", "Curly Brackets"],
+    answer: 1,
+  },
+
+  {
+    question: "Arrays in JavaScript can be used to store _______.",
+    choices: [
+      "Numbers and Strings",
+      "Other Arrays",
+      "Booleans",
+      "All of the above",
+    ],
+    answer: 0,
+  },
+
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "Terminal / Bash", "For Loops", "Console.log"],
+    answer: 3,
+  },
+];
